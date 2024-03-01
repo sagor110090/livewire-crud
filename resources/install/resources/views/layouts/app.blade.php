9b6e076f1ebbba966b4c4b72df113b65ca850a37
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2024-03-02 03:26:09 +0600

    updated

--- resources/install/resources/views/layouts/app.blade.php
+++ resources/install/resources/views/layouts/app.blade.php
@@ -1,36 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-slate-200 font-roboto">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <wireui:scripts />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
 
-            <div class="flex overflow-hidden flex-col flex-1">
-                @include('layouts.header')
+<body>
+    <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-slate-200 font-roboto">
+        @include('layouts.navigation')
 
-                <main class="overflow-y-auto overflow-x-hidden flex-1 bg-slate-200">
-                    <div class="container px-6 py-8 mx-auto">
-                        @if (isset($header))
-                            <h3 class="mb-4 text-3xl font-medium text-gray-700">
-                                {{ $header }}
-                            </h3>
-                        @endif
+        <div class="flex flex-col flex-1 overflow-hidden">
+            @include('layouts.header')
 
-                        {{ $slot }}
-                    </div>
-                </main>
-            </div>
+            <main class="flex-1 overflow-x-hidden overflow-y-auto bg-slate-200">
+                <div class="container px-6 py-8 mx-auto">
+                    @if (isset($header))
+                        <h3 class="mb-4 text-3xl font-medium text-gray-700">
+                            {{ $header }}
+                        </h3>
+                    @endif
+
+                    {{ $slot }}
+                </div>
+            </main>
         </div>
-    </body>
+    </div>
+    @livewireScripts
+    @livewire('wire-elements-modal')
+    @stack('scripts')
+    <x-livewire-alert::scripts />
+</body>
+
 </html>
